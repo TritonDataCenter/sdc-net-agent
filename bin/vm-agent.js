cbe4ba81131617c280b9d2bf5c80d4017a045a01
Andres Rodriguez <andres@joyent.com>
2014-05-28 17:34:23 -0700

    ZAPI-528: vm-agent should get VMAPI's URL from CN config

--- bin/vm-agent.js
+++ bin/vm-agent.js
@@ -5,6 +5,7 @@
  */
 
 var bunyan = require('bunyan');
+var async = require('async');
 var execFile = require('child_process').execFile;
 var VM = require('/usr/vm/node_modules/VM');
 
@@ -14,49 +15,80 @@ var logger = bunyan.createLogger({ name: 'vm-agent', level: logLevel });
 var VmAgent = require('../lib/vm-agent');
 var UpdateAgent = require('../lib/update-agent');
 
-var config = {
-    url: (process.env.VMAPI_URL || 'http://vmapi.coal.joyent.us'),
-    log: logger
-};
+var config = { log: logger };
+var sdcConfig;
+var sysinfo;
 
 process.on('uncaughtException', function (e) {
     console.error('uncaught exception:' + e.message);
     console.log(e.stack);
 });
 
+function loadConfig(callback) {
+    execFile('/bin/bash', ['/lib/sdc/config.sh', '-json'],
+        function _loadConfig(err, stdout, stderr) {
+            if (err) {
+                logger.fatal(err, 'Could not load config: ' + stderr);
+                return callback(err);
+            }
+
+            try {
+                sdcConfig = JSON.parse(stdout); // intentionally global
+            } catch (e) {
+                logger.fatal(e, 'Could not parse config: ' + e.message);
+                return callback(e);
+            }
+
+            return callback(null);
+        });
+}
+
 // Run the sysinfo script and return the captured stdout, stderr, and exit
 // status code.
 function loadSysinfo(callback) {
-    execFile('/usr/bin/sysinfo', [], function (exitStatus, stdout, stderr) {
-        if (exitStatus) {
-            return callback(new Error(stderr), exitStatus, stdout, stderr);
+    execFile('/usr/bin/sysinfo', [], function (err, stdout, stderr) {
+        if (err) {
+            logger.fatal('Could not load sysinfo: ' + stderr.toString());
+            return callback(err);
         }
 
-        return callback(
-            undefined, exitStatus,
-            stdout.toString().trim(), stderr.toString().trim());
+        try {
+            sysinfo = JSON.parse(stdout);
+        } catch (e) {
+            logger.fatal(e, 'Could not parse sysinfo: ' + e.message);
+            return callback(e);
+        }
+
+        return callback(null);
     });
 }
+
 
 var updateAgent;
 var VmAgent;
 
-loadSysinfo(function (error, exitStatus, stdout, stderr) {
-    if (error) {
-        console.error('sysinfo error: ' + stderr.toString());
+async.waterfall([
+    loadConfig,
+    loadSysinfo
+], function (err) {
+    if (err) {
+        log.fatal('Failed to initialize vm-agent configuration');
         process.exit(1);
     }
 
-    // output of sysinfo is a JSON object
-    var sysinfo = JSON.parse(stdout);
-
-    // Use the UUID param to uniquely identify this machine on AMQP.
     if (!sysinfo.UUID) {
-        console.error('Could not find "UUID" in `sysinfo` output.');
+        log.fatal('Could not find "UUID" in `sysinfo` output.');
         process.exit(1);
     }
 
     config.uuid = sysinfo.UUID;
+    var vmapi_url = 'http://' + sdcConfig.vmapi_domain;
+    config.url = (process.env.VMAPI_URL || vmapi_url);
+
+    if (!config.url) {
+        log.fatal('config.url is required');
+        process.exit(1);
+    }
 
     var updateAgent = new UpdateAgent(config);
     config.updateAgent = updateAgent;
@@ -64,4 +96,3 @@ loadSysinfo(function (error, exitStatus, stdout, stderr) {
     var vmagent = new VmAgent(config);
     vmagent.start();
 });
-
